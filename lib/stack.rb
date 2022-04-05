881ce0f8f6283765e168d4676910bd0b268829ac
Kaleb <kaleb.franken7@gmail.com>
2022-04-05 15:39:58 -0600

    commit tree 08b5bd73292d65fab61baf3b1d153497d4cd1b40

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
 # your code here
+class Stack
+    def initialize(limit = nil)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(t)
+        if @stack.length >== limit do
+            raise "Stack Overflow"
+        else do
+            @stack.push(t)
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+end
